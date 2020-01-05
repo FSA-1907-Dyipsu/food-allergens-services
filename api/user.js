51c7c5c95ab758abdc8d84102cc3a1f5a2a05141
Kristina Cayetano <kma.cayetano@gmail.com>
2020-01-05 00:51:44 -0500

    refactor user

--- api/user.js
+++ api/user.js
@@ -3,7 +3,9 @@ const {Users} = require('../db/index').models
 
 //get user
 router.get('/', (req, res) => {
-  res.send(req.user);
+  const { user } = req
+  if (user) res.send(user)
+  else res.status(404).send('User not found')
 });
 
 //get specific user
